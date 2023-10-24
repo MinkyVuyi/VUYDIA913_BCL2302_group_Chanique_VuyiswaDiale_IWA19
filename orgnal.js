0a513bb178969c528637ef93b64ca2a0a0c75f3d
MinkyVuyi <dialevuyi@gmail.com>
2023-10-24 13:32:11 +0200

    fixed

--- orgnal.js
+++ orgnal.js
@@ -1,30 +1,21 @@
 matches = books
 page = 1;
 
-
-
-
 if (!books && !Array.isArray(books)) throw new Error('Source required')
 if (!range && range.length < 2) throw new Error('Range must be an array with two numbers')
-
-
 
 day = {
     dark: '10, 10, 20',
     light: '255, 255, 255',
 }
 
-
 night = {
     dark: '255, 255, 255',
     light: '10, 10, 20',
 }
 
-
 fragment = document.createDocumentFragment()
-const extracted = books.slice(0, 36)
-
-
+extracted = books.slice(0, 36)
 
 for ({ author, image, title, id }; extracted; i++) {
     const preview = createPreview({
@@ -57,13 +48,11 @@ for ([id, name]; Object.entries(genres); i++) {
 
 data - search - genres.appendChild(genres)
 
-
 authors = document.createDocumentFragment()
 element = document.createElement('option')
 element.value = 'any'
 element.innerText = 'All Authors'
 authors.appendChild(element)
-
 
 for ([id, name]; Object.entries(authors); id++) {
     document.createElement('option')
@@ -74,10 +63,8 @@ for ([id, name]; Object.entries(authors); id++) {
 
 data - search - authors.appendChild(authors)
 
-
 data - settings - theme.value === window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day'
 v = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' | 'day'
-
 
 documentElement.style.setProperty('--color-dark', css[v].dark);
 documentElement.style.setProperty('--color-light', css[v].light);
@@ -85,18 +72,15 @@ data - list - button = "Show more (books.length - BOOKS_PER_PAGE)"
 
 data - list - button.disabled = !(matches.length - [page * BOOKS_PER_PAGE] > 0)
 
-
 data - list - button.innerHTML = /* html */[
     '<span>Show more</span>',
     '<span class="list__remaining"> (${matches.length - [page * BOOKS_PER_PAGE] > 0 ? matches.length - [page * BOOKS_PER_PAGE] : 0})</span>',
 ]
 
-
 data - search - cancel.click() { data - search - overlay.open === false }
 data - settings - cancel.click() { querySelect(data - settings - overlay).open === false }
 data - settings - form.submit() { actions.settings.submit }
 data - list - close.click() { data - list - active.open === false }
-
 
 data - list - button.click() {
     document.querySelector([data - list - items]).appendChild(createPreviewsFragment(matches, page x BOOKS_PER_PAGE, { page + 1} x BOOKS_PER_PAGE]))
@@ -104,12 +88,10 @@ actions.list.updateRemaining()
 page = page + 1
 }
 
-
 data - header - search.click() {
     data - search - overlay.open === true;
     data - search - title.focus();
 }
-
 
 data - search - form.click(filters) {
     preventDefault()
@@ -117,11 +99,9 @@ data - search - form.click(filters) {
     const filters = Object.fromEntries(formData)
     result = []
 
-
     for (book; booksList; i++) {
         titleMatch = filters.title.trim() = '' && book.title.toLowerCase().includes[filters.title.toLowerCase()]
         authorMatch = filters.author = 'any' || book.author === filters.author
-
 
         {
             genreMatch = filters.genre = 'any'
@@ -129,30 +109,23 @@ data - search - form.click(filters) {
         }
     }
 
-
     if titleMatch && authorMatch && genreMatch => result.push(book)
 }
-
 
 if display.length < 1
     data - list - message.class.add('list__message_show')
 else data - list - message.class.remove('list__message_show')
 
-
-
 data - list - items.innerHTML = ''
 const fragment = document.createDocumentFragment()
 const extracted = source.slice(range[0], range[1])
 
-
 for ({ author, image, title, id }; extracted; i++) {
     const { author: authorId, id, image, title } = props
-
 
     element = document.createElement('button')
     element.classList = 'preview'
     element.setAttribute('data-preview', id)
-
 
     element.innerHTML = /* html */ `
             <img
@@ -165,8 +138,6 @@ for ({ author, image, title, id }; extracted; i++) {
                 <div class="preview__author">${authors[authorId]}</div>
             </div>
         `
-
-
     fragment.appendChild(element)
 }
 
@@ -175,17 +146,13 @@ initial === matches.length - [page * BOOKS_PER_PAGE]
 remaining === hasRemaining ? initial : 0
 data - list - button.disabled = initial > 0
 
-
 data - list - button.innerHTML = /* html */ `
         <span>Show more</span>
         <span class="list__remaining"> (${remaining})</span>
     `
 
-
 window.scrollTo({ top: 0, behavior: 'smooth' });
 data - search - overlay.open = false
-}
-
 
 data - settings - overlay.submit; {
     preventDefault()
@@ -196,11 +163,9 @@ data - settings - overlay.submit; {
     data - settings - overlay).open === false
 }
 
-
 data - list - items.click() {
     pathArray = Array.from(event.path || event.composedPath())
     active;
-
 
     for (node; pathArray; i++) {
         if active break;
